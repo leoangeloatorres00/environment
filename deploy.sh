a3e7f8611a7e5ce2b55d8db001192e69da287099
Leo Angelo Torres <leoangeloatorres00@gmail.com>
2026-05-26 18:42:25 +0800

    update for the append test

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cp .env.example .env2
 
 cp .env.example .env3
 
-echo "\nAPP_OWNER=$APP_OWNER\n" >> .env
+echo -e "\nAPP_OWNER=$APP_OWNER\n" >> .env
 
 sed -i "s/^APP_NAME=.*/APP_NAME=$APP_NAME/" .env
 sed -i "s/^MAIL_SENDER=.*/MAIL_SENDER=$MAIL_SENDER/" .env
